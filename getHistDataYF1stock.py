2fda7e056b4fd2e2386c33d695e4e91a38985fc7
ClimberMel <climbermel@gmail.com>
2024-12-13 16:12:42 -0800

    Update getHistDataYF1stock.py

--- getHistDataYF1stock.py
+++ getHistDataYF1stock.py
@@ -6,5 +6,7 @@ import yfinance as yf
 aapl_df = yf.download('AAPL')
 
 df = pd.DataFrame(aapl_df)
+
+#If you uncomment the line below it wil write the data to a csv file in the current folder
 #df.to_csv('AAPL.csv')
 print(df)
